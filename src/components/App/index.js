4783a0c9b50f5bf4f301b0f054902bd2e19fe2a3
Frank LaNasa <frank@placepass.com>
2018-08-14 20:23:26 -0400

    Add node-sass-chokidar for css preprocessing

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,8 +4,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p className="App-intro">
+      <div className="app">
+        <p className="app-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
